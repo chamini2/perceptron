aeb35eca78e73c9f98a8470ccfda8e4965ec50f3
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-10-06 03:47:40 -0430

    Pregunta 1 completa

--- perceptrón.hs
+++ perceptrón.hs
@@ -1,75 +1,143 @@
 module Main where
 
-import           Control.Arrow         ((&&&))
-import           Control.Monad.Reader  (ReaderT, asks, runReaderT, liftIO)
-import           Data.Foldable         (foldlM)
-import           Foreign.Marshal.Utils (fromBool)
+import           Control.Arrow                          (second, (&&&))
+import           Control.Monad                          (forM_, liftM, when)
+import           Control.Monad.Reader                   (ReaderT, asks, liftIO,
+                                                         runReaderT)
+import           Data.Foldable                          (foldlM)
+import           Data.List                              (genericLength)
+import           Data.Traversable                       (forM)
+import           Graphics.Rendering.Chart.Backend.Cairo
+import           Graphics.Rendering.Chart.Easy
+import           System.Directory                       (doesFileExist,
+                                                         removeFile)
+
+average :: (Real a, Fractional b) => [a] -> b
+average xs = realToFrac (sum xs) / genericLength xs
+
+fromBool :: Num a => Bool -> a
+fromBool True  = 1
+fromBool False = 0
+
+--------------------------------------------------------------------------------
+
+-- 1.a)
+pregunta1_a :: IO ()
+pregunta1_a = do
+    doesFileExist "pregunta1_a.txt" >>= flip when (removeFile "pregunta1_a.txt")
+    erroress <- forM [(conjunto_and, "AND"), (conjunto_or, "OR"), (conjunto_xor, "XOR")] $ \(conjunto, nombre) -> do
+        (pesos, errores) <- correrLector infoInicial conjunto
+        appendFile "pregunta1_a.txt" $ "pesos       " ++ nombre ++ ": " ++ show pesos            ++ "\n"
+        appendFile "pregunta1_a.txt" $ "iteraciones " ++ nombre ++ ": " ++ show (length errores) ++ "\n"
+        appendFile "pregunta1_a.txt" $ "errores     " ++ nombre ++ ": " ++ show errores          ++ "\n\n"
+        return (errores, nombre)
+    -- Gráfica
+    toFile def "pregunta1_a.png" $ do
+        layout_title .= "Error para perceptrón"
+        forM_ erroress $ \(errores, nombre) -> do
+            plot (line nombre  [errores])
+
+-- 1.b)
+pregunta1_b :: IO ()
+pregunta1_b = do
+    doesFileExist "pregunta1_b.txt" >>= flip when (removeFile "pregunta1_b.txt")
+    forM_ [0.01, 0.1, 0.2, 0.5, 0.99] $ \t -> do
+        appendFile "pregunta1_b.txt" $ "TASA: " ++ show t ++ "\n"
+
+        forM [(conjunto_and, "AND"), (conjunto_or, "OR")] $ \(conjunto, nombre) -> do
+            (pesos, errores) <- correrLector infoInicial conjunto
+            appendFile "pregunta1_b.txt" $ "\tpesos       " ++ nombre ++ ": " ++ show pesos            ++ "\n"
+            appendFile "pregunta1_b.txt" $ "\titeraciones " ++ nombre ++ ": " ++ show (length errores) ++ "\n"
+            appendFile "pregunta1_b.txt" $ "\terrores     " ++ nombre ++ ": " ++ show errores          ++ "\n\n"
+
+
+--------------------------------------------------------------------------------
 
 data Información = Info
     { umbral :: Double
     , tasa   :: Double
     }
 
-informaciónInicial :: Información
-informaciónInicial = Info 0.5 0.1
+infoInicial :: Información
+infoInicial = Info 0.5 0.1
 
 type Lector = ReaderT Información IO
 
-correrLector :: [([Double], Double)] -> IO [Double]
-correrLector conjunto = do
-    vectorW <- correrRecursivo 100 pesosIniciales conjunto
-    mapM_ showTuple $ zip [0..] vectorW
-    return vectorW
-    where
-        showTuple (n,x) = putStrLn $ "W" ++ show n ++ " = " ++ show x
+--------------------------------------------------------------------------------
 
-correrRecursivo :: Int -> [Double] -> [([Double], Double)] -> IO [Double]
-correrRecursivo it pesos conjunto =
+correrLector :: Información -> Conjunto -> IO ([Double], [(Double, Double)])
+correrLector info = liftM (second func) . correrRecursivo 10 pesosIniciales [] info
+    where
+        func = zip [1..] . map sum . reverse
+
+correrRecursivo :: Int -> [Double] -> [[Double]] -> Información -> Conjunto -> IO ([Double], [[Double]])
+correrRecursivo it pesos errores info conjunto =
     if it > 0 then do
-        pesos' <- runReaderT (perceptrón pesos conjunto) informaciónInicial
-        correrRecursivo (it - 1) pesos' conjunto
-    else return pesos
+        (pesos', errores') <- runReaderT (perceptrón pesos conjunto) info
+        -- cuando no hubo errores
+        if all (==0) errores'
+          then return (pesos', errores' : errores)
+          else correrRecursivo (pred it) pesos' (errores' : errores) info conjunto
+    else return (pesos, errores)
 
 --------------------------------------------------------------------------------
 
 pesosIniciales :: [Double]
-pesosIniciales = [0, 0, 0]
+pesosIniciales = repeat 0
 
 ----------------------------------------
 -- Conjuntos de aprendizaje
 
-conjunto_nand :: [([Double], Double)]
-conjunto_nand = [ ([1, 0, 0], 1)
-                , ([1, 0, 1], 1)
-                , ([1, 1, 0], 1)
-                , ([1, 1, 1], 0)
-                ]
+type Conjunto = [([Bool], Bool)]
 
-conjunto_and :: [([Double], Double)]
-conjunto_and = [ ([1, 0, 0], 0)
-               , ([1, 0, 1], 0)
-               , ([1, 1, 0], 0)
-               , ([1, 1, 1], 1)
-               ]
+conjunto_and :: Conjunto
+conjunto_and = [ ([ True, False, False], False)
+               , ([ True, False,  True], False)
+               , ([ True,  True, False], False)
+               , ([ True,  True,  True],  True) ]
 
-conjunto_or :: [([Double], Double)]
-conjunto_or = [ ([1, 0, 0], 0)
-              , ([1, 0, 1], 1)
-              , ([1, 1, 0], 1)
-              , ([1, 1, 1], 1)
-              ]
+conjunto_nand :: Conjunto
+conjunto_nand = [ ([ True, False, False],  True)
+                , ([ True, False,  True],  True)
+                , ([ True,  True, False],  True)
+                , ([ True,  True,  True], False) ]
+
+conjunto_or :: Conjunto
+conjunto_or = [ ([ True, False, False], False)
+              , ([ True, False,  True],  True)
+              , ([ True,  True, False],  True)
+              , ([ True,  True,  True],  True) ]
+
+conjunto_xor :: Conjunto
+conjunto_xor = [ ([ True, False, False], False)
+               , ([ True, False,  True],  True)
+               , ([ True,  True, False],  True)
+               , ([ True,  True,  True], False) ]
 
 --------------------------------------------------------------------------------
 
-perceptrón :: [Double] -> [([Double], Double)] -> Lector [Double]
-perceptrón = foldlM func
+perceptrón :: [Double] -> Conjunto -> Lector ([Double], [Double])
+perceptrón pesos conjunto = foldlM func (pesos, []) conjunto
     where
-        func :: [Double] -> ([Double], Double) -> Lector [Double]
-        func vectorW (vectorX, z) = do
+        func :: ([Double], [Double]) -> ([Bool], Bool) -> Lector ([Double], [Double])
+        func (ws, errores) (xs, z) = do
             (u, t) <- asks (umbral &&& tasa)
-            let s = sum $ zipWith (*) vectorX vectorW
-                n = fromBool (s > u)                    -- True == 1, False == 0
-                e = z - n
+            let s = sum $ zipWith (\x w -> fromBool x * w) xs ws
+                n = fromBool $ s > u
+                e = fromBool z - n
                 d = t * e
-            -- liftIO . putStrLn $ "error = " ++ show e
-            return $ zipWith (+) vectorW $ map (*d) vectorX     -- nuevo vectorW
+                -- (nuevo ws, error de este caso)
+            return (zipWith (+) ws $ map ((*d) . fromBool) xs, e : errores)
+
+delta :: [Double] -> Conjunto -> Lector ([Double], [Double])
+delta pesos conjunto = foldlM func (pesos, []) conjunto
+    where
+        func :: ([Double], [Double]) -> ([Bool], Bool) -> Lector ([Double], [Double])
+        func (ws, errores) (xs, z) = do
+            (u, t) <- asks (umbral &&& tasa)
+            let s = sum $ zipWith (\x w -> fromBool x * w) xs ws
+                n = fromBool $ s > u
+                e = fromBool z - n
+                d = t * e
+                -- (nuevo ws, error de este caso)
+            return (zipWith (+) ws $ map ((*d) . fromBool) xs, e : errores)
